50ba91dc8e5d7f979617f6596051198c71354bd5
Leonardo <leonardo.paiva@mercadolivre.com>
2022-10-31 21:30:13 -0300

    Java aula 3

---
+++ Java/Aula3/aula_III/src/aula_III/JavaAula3.java
@@ -0,0 +1,26 @@
+package aula_III;
+
+import java.util.Scanner;
+
+public class JavaAula3 {
+
+	public static Scanner Leia = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int numero,soma = 0, cont = 0;
+		
+		do {
+		System.out.println("Digite um numero: ");
+		numero = Leia.nextInt();
+		if(numero > 0) {
+			soma = numero + soma;
+			cont++;
+		}
+		}while(numero > 0);
+		System.out.println("Contagem terminada, A soma dos números positivos é "+ soma);
+		System.out.println(numero);
+
+	}
+
+}
